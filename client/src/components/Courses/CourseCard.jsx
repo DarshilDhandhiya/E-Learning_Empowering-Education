339a0587952cb88e1073eaa127db83de6e6c7dd5
DarshilDhandhiya <darshil.dhandhiya03@gmail.com>
2023-09-24 23:50:41 +0530

    Course Responsive

--- client/src/components/Courses/CourseCard.jsx
+++ client/src/components/Courses/CourseCard.jsx
@@ -174,66 +174,63 @@ export const coursesCard = [
     }
   ]
 
-const HAbout = () => {
-  return (
-    <>
-      <section className='homeAbout'>
-        <div className='container'>
-          <Heading subtitle='our courses' title='explore our popular online courses' />
-
-          <div className='coursesCard'>
-            {/* copy code form  coursesCard */}
-            <div className='grid2'>
-              {coursesCard.slice(0, 3).map((val) => (
-                <div className='items'>
-                  <div className='content flex'>
-                    <div className='left'>
-                      <div className='img'>
-                        <img src={val.cover} alt='' />
+  const HAbout = () => {
+    return (
+      <>
+        <section className="homeAbout">
+          <div className="container">
+            <Heading subtitle="our courses" title="explore our popular online courses" />
+  
+            <div className="coursesCard">
+              <div className="grid2">
+                {coursesCard.slice(0, 3).map((val) => (
+                  <div className="items" key={val.id}>
+                    <div className="content flex">
+                      <div className="left">
+                        <div className="img">
+                          <img src={val.cover} alt={val.coursesName} />
+                        </div>
                       </div>
-                    </div>
-                    <div className='text'>
-                      <h1>{val.coursesName}</h1>
-                      <div className='rate'>
-                        <i className='fa fa-star'></i>
-                        <i className='fa fa-star'></i>
-                        <i className='fa fa-star'></i>
-                        <i className='fa fa-star'></i>
-                        <i className='fa fa-star'></i>
-                        <label htmlFor=''>(5.0)</label>
-                      </div>
-                      <div className='details'>
-                        {val.courTeacher.map((details) => (
-                          <>
-                            <div className='box'>
-                              <div className='dimg'>
-                                <img src={details.dcover} alt='' />
+                      <div className="text">
+                        <h1>{val.coursesName}</h1>
+                        <div className="rate">
+                          <i className="fa fa-star"></i>
+                          <i className="fa fa-star"></i>
+                          <i className="fa fa-star"></i>
+                          <i className="fa fa-star"></i>
+                          <i className="fa fa-star"></i>
+                          <label htmlFor="">(5.0)</label>
+                        </div>
+                        <div className="details">
+                          {val.courTeacher.map((details) => (
+                            <div className="box" key={details.name}>
+                              <div className="dimg">
+                                <img src={details.dcover} alt={details.name} />
                               </div>
-                              <div className='para'>
+                              <div className="para">
                                 <h4>{details.name}</h4>
                               </div>
                             </div>
-                            <span>{details.totalTime}</span>
-                          </>
-                        ))}
+                          ))}
+                          <span>{val.courTeacher[0].totalTime}</span>
+                        </div>
                       </div>
                     </div>
+                    <div className="price">
+                      <h3>
+                        {val.priceAll} / {val.pricePer}
+                      </h3>
+                    </div>
+                    <button className="outline-btn">ENROLL NOW !</button>
                   </div>
-                  <div className='price'>
-                    <h3>
-                      {val.priceAll} / {val.pricePer}
-                    </h3>
-                  </div>
-                  <button className='outline-btn'>ENROLL NOW !</button>
-                </div>
-              ))}
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-        <OnlineCourses />
-      </section>
-    </>
-  )
-}
-
-export default HAbout
+          <OnlineCourses />
+        </section>
+      </>
+    );
+  };
+  
+  export default HAbout;
