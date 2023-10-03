0ea1438af65724d19c6f08eac2970678f0733ea3
Darshil Dhandhiya <darshil.dhandhiya03@gmail.com>
2023-10-03 22:39:54 +0530

    Admission

--- client/src/components/Admission/Admission.jsx
+++ client/src/components/Admission/Admission.jsx
@@ -1,12 +1,121 @@
-import React from 'react'
-import Back from "../Common/back/Back"
+import React, { useState } from 'react';
+import Back from '../Common/back/Back';
 
 const Admission = () => {
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    address: '',
+    selectedProgram: 'Select Program',
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // You can perform form submission logic here
+    console.log(formData);
+  };
+
   return (
     <div>
-      <Back title='Admission' />
+      <Back title="Admission" />
+      <div className="container mx-auto p-4">
+        <h2 className="text-2xl font-semibold mb-4">Admission Form</h2>
+        <form onSubmit={handleSubmit} className="max-w-md mx-auto">
+          <div className="mb-4">
+            <label htmlFor="firstName" className="block mb-2">First Name:</label>
+            <input
+              type="text"
+              id="firstName"
+              name="firstName"
+              value={formData.firstName}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="lastName" className="block mb-2">Last Name:</label>
+            <input
+              type="text"
+              id="lastName"
+              name="lastName"
+              value={formData.lastName}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-2">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="phoneNumber" className="block mb-2">Phone Number:</label>
+            <input
+              type="tel"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="address" className="block mb-2">Address:</label>
+            <textarea
+              id="address"
+              name="address"
+              value={formData.address}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="selectedProgram" className="block mb-2">Select Program:</label>
+            <select
+              id="selectedProgram"
+              name="selectedProgram"
+              value={formData.selectedProgram}
+              onChange={handleChange}
+              className="w-full p-2 border rounded"
+              required
+            >
+              <option value="Select Program">Select Program</option>
+              <option value="Program A">Program A</option>
+              <option value="Program B">Program B</option>
+              <option value="Program C">Program C</option>
+            </select>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 text-white font-semibold py-2 px-4 rounded hover:bg-blue-600"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Admission
+export default Admission;
